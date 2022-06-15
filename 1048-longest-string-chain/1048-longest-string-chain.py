dc789f52443818c167dc2100e208930e9d7d9954
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-06-15 18:04:21 +0530

    Time: 191 ms (72.50%), Space: 16.8 MB (17.72%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        s = set(words)
+        memo = {}
+
+        def rec(word):
+            if word not in s: return 0
+            if word in memo:
+                return memo[word]
+            else:
+                mx=0
+                for i in range(len(word)):
+                    mx = max(mx,rec(word[:i]+word[i+1:])+1)
+                memo[word]=mx
+            return mx
+        for w in words:
+            rec(w)
+        return max(memo.values())
