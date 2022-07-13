0e68ec5b6e57a0046adb5839f0e1bf5721605945
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-07-13 14:57:56 +0530

    Time: 55 ms (47.23%), Space: 14.1 MB (84.23%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -0,0 +1,72 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+import collections
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        res = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            level = []
+            for i in range(len(q)):
+                node =q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '''
+        ans=[]
+        q=collections.deque()
+        q.append(root)
+        while q:
+            ql=len(q)
+            a=[]
+            for i in range(ql):
+                node=q.popleft()
+                if node:
+                    a.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if a:
+                ans.append(a)
+        return ans
+'''
+        # ans = []
+        # q = collections.deque()
+        # q.append(root)
+        # while q:
+        #     q1 = len(q)
+        #     a = []
+        #     for i in range(q1):
+        #         node = q.popleft()
+        #         if node:
+        #             a.append(node.val)
+        #             q.append(node.left)
+        #             q.append(node.right)
+        #     if a:
+        #         ans.append(a)
+        # return ans
+        
