bbb36d8f2be12232bc8a94e58abda5806b2564b7
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-07-24 14:37:17 +0530

    Time: 335 ms (16.95%), Space: 20.3 MB (98.38%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,50 +1,21 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not len(matrix) or not len(matrix[0]):
-			# Quick response for empty matrix
-            return False
         
-        h, w = len(matrix), len(matrix[0])
-        
-        # Start adaptive search from bottom left corner
-        y, x = h-1, 0
-        
-        while True:
-            
-            if y < 0 or x >= w:
-                break
-            
-            current = matrix[y][x]
-            
-            if target < current:
-                # target is smaller, then go up
-                y -= 1
-            
-            elif target > current:
-                # target is larger, then go right
-                x += 1
-            
-            else:
-                # hit target
-                return True
-            
-        return False
-        '''
+        # '''
         m = len(matrix)
         n = len(matrix[0])
         if m==0:
             return False
-        
         x,y = m-1,0
         while True:
-            
-            if x<0 or y>=m:
+            if x<0 or y>=n:
                 break
-            if matrix[x][y]==target:
-                return True
-            elif matrix[x][y] > target:
+            if matrix[x][y] > target:
                 x-=1
-            else:
+            elif matrix[x][y]< target:
                 y+=1
+            else:
+                return True
         return False
-        '''
+            
+        # '''
