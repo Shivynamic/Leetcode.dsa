3ae055c0a1dc47057ea13dd30da8797b861e76c6
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-07-30 02:17:22 +0530

    Time: 45 ms (70.39%), Space: 13.9 MB (63.51%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,7 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         
+        
         """
+        i,mid =0,0
+        j =len(nums)-1
+        while mid<= j:
+            if nums[mid]==0:
+                nums[i],nums[mid] = nums[mid],nums[i]
+                i+=1
+                mid+=1
+            elif nums[mid]==2:
+                nums[j],nums[mid] = nums[mid],nums[j]
+                j-=1
+            else:
+                mid+=1
+        
+                
+        '''
         a = nums.count(0)
         b = nums.count(1)
         i=0
@@ -18,3 +34,4 @@ class Solution:
         while i<len(nums):
             nums[i] =2
             i+=1
+        '''
