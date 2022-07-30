dee12b5e1e852e3614633bb0bbd8c40f8f4134c6
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-07-30 23:48:34 +0530

    Time: 1516 ms (34.34%), Space: 18.6 MB (54.67%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -4,34 +4,16 @@ def check(w,d):
         d1[i]+=1
     
     for x in d:
-        # print(x)
-        # print(d[x],d1[x])
         if x not in d1 or d[x]>d1[x]:
             return False
         
     return True
 
-def checkk(w,d):
-    d1 = defaultdict(lambda:0)
-    for i in w:
-        d1[i]+=1
-    
-    for i in d:
-        dummy = defaultdict(lambda:0)
-        for x in i:
-            dummy[x]+=1
-        for k in dummy:
-            if k not in d1 or dummy[k]>d1[k]:
-                return False
-    return True
-    
-
-    
+ 
 
 
 from collections import defaultdict
 class Solution:
-    # @cache
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
         d = defaultdict(lambda:0)
         for i in words2:
@@ -45,6 +27,36 @@ class Solution:
             if check(w,d):
                 out.append(w)
         return out
+
+# def check(w,d):
+#     d1 = defaultdict(lambda:0)
+#     for i in w:
+#         d1[i]+=1
+    
+#     for x in d:
+#         if x not in d1 or d[x]>d1[x]:
+#             return False
+        
+#     return True
+
+ 
+
+
+# from collections import defaultdict
+# class Solution:
+#     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+#         d = defaultdict(lambda:0)
+#         for i in words2:
+#             for j in i:
+#                 if j not in d:
+#                     d[j] = i.count(j)
+#                 else:
+#                     d[j] = max(d[j], i.count(j))
+#         out = []
+#         for w in words1:
+#             if check(w,d):
+#                 out.append(w)
+#         return out
             
             
         
