10c3a6dc99d5141f42d664b262bb00a563bcb848
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-08-03 13:55:36 +0530

    Time: 32 ms (93.61%), Space: 13.9 MB (73.95%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,5 +1,36 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0]*n]*m
+        
+        for i in range(m):
+            dp[i][0]=1
+        for i in range(n):
+            dp[0][i]=1
+            
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
+        
+        
+        '''dp = [[-1]*n]*m
+        print(dp)
+        def help(i,j,m,n,dp):
+            if i>=m or j>=n:
+                return 0
+            if i==m-1 and j==n-1:
+                return 1
+            
+            
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            
+            dp[i][j] = help(i+1,j,m,n,dp)+help(i,j+1,m,n,dp)
+            return dp[i][j]
+        return help(0,0,m,n,dp)'''
+        
+        
+'''
         row = [1]*n
         for i in range(m-1):
             newRow= [1]*n
@@ -7,4 +38,16 @@ class Solution:
                 newRow[j] = newRow[j+1]+row[j]
             row = newRow
         return row[0]
+'''
         
+        
+        
+'''
+        def helper(i,j,m,n):
+            if i==m-1 and j==n-1:
+                return 1
+            if i>=m or j >=n:
+                return 0
+            return helper(i+1,j,m,n)+helper(i,j+1,m,n)
+        return helper(0,0,m,n)
+        '''
