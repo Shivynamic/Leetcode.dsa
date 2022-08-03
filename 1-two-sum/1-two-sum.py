1ee3a816607e43d3ed2b17eb285ae2fc8ae67e2c
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-08-03 21:50:34 +0530

    Time: 94 ms (68.13%), Space: 15.1 MB (50.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,16 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen={}
+        seen = {}
         for i in range(len(nums)):
-            s = target-nums[i]
-            if s in seen:
-                return [seen[s],i]
+            new = target - nums[i]
+            if new in seen:
+                return [seen[new] , i]
             else:
-                seen[nums[i]]=i
-
-#         for i in range(len(nums)):
-#             st = target - nums[i]
-#             if nums[i+1:] and st in nums[i+1:]:
-#                 return [i,i+nums[i+1:].index(st)+1]
-        
-        
+                seen[nums[i]]= i       
