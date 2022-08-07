25e1dd731d8d6fa00da85041428cce0299138b5f
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-08-07 08:38:47 +0530

    Time: 47 ms (73.04%), Space: 14.9 MB (46.74%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -9,16 +9,47 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        old = {None:None}
-        curr = head
-        while curr:
-            old[curr] = Node(curr.val)
-            curr = curr.next
+        itr = front = head
+        while itr:
+            front = itr.next
+            
+            copy = Node(itr.val)
+            itr.next = copy
+            copy.next = front
+            
+            itr = front
         
-        new = head
-        while new:
-            copy = old[new]
-            copy.next = old[new.next]
-            copy.random = old[new.random]
-            new=new.next
-        return old[head]
+        itr = head
+        while itr:
+            if itr.random:
+                itr.next.random = itr.random.next
+            itr = itr.next.next
+        
+        itr = head
+        dummy = Node(0)
+        copy = dummy
+        
+        while itr:
+            front = itr.next.next
+            copy.next = itr.next
+            copy = copy.next
+            itr.next = front
+            itr = front
+        
+        return dummy.next
+        
+            
+        
+#         old = {None:None}
+#         curr = head
+#         while curr:
+#             old[curr] = Node(curr.val)
+#             curr = curr.next
+        
+#         new = head
+#         while new:
+#             copy = old[new]
+#             copy.next = old[new.next]
+#             copy.random = old[new.random]
+#             new=new.next
+#         return old[head]
