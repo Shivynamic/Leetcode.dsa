f1c2c51d37753949deb40e06e373853ad6dc35a1
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-08-24 10:10:23 +0530

    Time: 57 ms (86.25%), Space: 14.2 MB (57.41%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,6 +1,23 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        num1ind = {n:i for i,n in enumerate(nums1)}
+        lookupind = {n:i for i,n in enumerate(nums1)}
+        res = [-1]*len(nums1)
+        
+        stack = []
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur>stack[-1]:
+                val = stack.pop()
+                ind = lookupind[val]
+                res[ind] = cur
+            
+            if cur in lookupind:
+                stack.append(cur)
+        return res
+        
+        
+        
+        '''num1ind = {n:i for i,n in enumerate(nums1)}
         res = [-1]*len(nums1)
         
         for i in range(len(nums2)):
@@ -11,4 +28,4 @@ class Solution:
                 if nums2[j]>nums2[i]:
                     res[num1ind[nums2[i]]] = nums2[j] 
                     break
-        return res
+        return res'''
