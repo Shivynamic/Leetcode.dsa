dfc9961e49261e7825a87f5f43a1396b7aa12ba3
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-08-29 13:43:13 +0530

    Time: 555 ms (34.20%), Space: 21.9 MB (27.34%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,7 +1,5 @@
-import collections
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
         if not grid:
             return 0
         
@@ -35,48 +33,3 @@ class Solution:
                     islands+=1
         return islands
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         if not grid:
-#             return 0
-        
-#         row,col = len(grid),len(grid[0])
-        
-#         xp = [-1,1,0,0]
-#         yp = [0,0,-1,1]
-        
-#         def bfs(i,j):
-#             que = collections.deque()
-#             que.append((i,j))
-            
-#             grid[i][j]="-1"
-            
-#             while que:
-#                 i,j = que.popleft()
-#                 for k in range(4):
-#                     x = i+xp[k]
-#                     y = j+ yp[k]
-                    
-#                     if x>=0 and x< row and y>=0 and y<col and grid[x][y]=="1":
-#                         grid[x][y]="-1"
-#                         que.append((x,y))
-            
-                        
-#         count =0
-#         for i in range(row):
-#             for j in range(col):
-#                 if grid[i][j]=="1":
-#                     bfs(i,j)
-#                     # print("HI")
-#                     count+=1
-#         return count
