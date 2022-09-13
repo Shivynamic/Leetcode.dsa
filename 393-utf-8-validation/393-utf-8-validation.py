cd33c2c461acaab7b38691bbcee11255d0f1fee0
Shivynamic <60092469+Shivynamic@users.noreply.github.com>
2022-09-13 21:23:24 +0530

    Time: 182 ms (55.12%), Space: 14.1 MB (70.76%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+#         def check(n):
+#             a = str(bin(n))
+#             l = a.split("0")
+#             c = l[i].count("1")
+#             if len(l)==c:
+#                 return True 
+#             return False
+        
+        def countOnes(num):
+            count =0
+            for i in range(7,-1,-1):
+                if num & (1<<i):
+                    count+=1
+                else:
+                    break
+            return count
+        count =0 
+        for d in data:
+            if not count:
+                count = countOnes(d)
+                if count==0:
+                    continue
+                if count==1 or count>4:
+                    return False
+                count-=1
+            else:
+                count-=1
+                if countOnes(d)!=1:
+                    return False
+        return count==0
